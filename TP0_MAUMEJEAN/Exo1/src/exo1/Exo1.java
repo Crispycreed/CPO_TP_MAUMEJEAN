4acd366e46e32531359eee3a8c75f367b8adfc8b
Denis MAUMEJEAN <denis.maumejean@epfedu.fr>
2023-09-26 11:40:31 +0200

    créé un nv exo apres prblm

---
+++ TP0_MAUMEJEAN/Exo1/src/exo1/Exo1.java
@@ -0,0 +1,19 @@
+//MAUMEJEAN DENIS
+//TDB
+
+package exo1;
+
+/**
+ *
+ * @author MAUMEJEAN DENIS
+ */
+public class Exo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
