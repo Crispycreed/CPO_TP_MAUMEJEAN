7fcf4f89b6b42ccf1c0cb8456cd326572a15da3f
Denis MAUMEJEAN <denis.maumejean@epfedu.fr>
2023-10-18 16:51:27 +0200

    debut

---
+++  TP3_Heroic_Fantasy_MAUMEJEAN/src/tp3_heroic_fantasy_maumejean/Arme.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp3_heroic_fantasy_maumejean;
+
+/**
+ *
+ * @author MAUMEJEAN DENIS
+ */
+public class Arme {
+    String Nom ;
+    int Attaque ;
+
+
+    public Arme(String Nom, int Attaque) {
+        this.Nom = Nom;
+        this.Attaque = Attaque;
+
+    @Override
+    public String toString() {
+        return "Arme{" + "Nom=" + Nom + ", Attaque=" + Attaque +"}";
+    }
+}
