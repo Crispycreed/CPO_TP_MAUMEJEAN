07c627aa4e043d5e44790d2cff9fa0468acff572
Denis MAUMEJEAN <denis.maumejean@epfedu.fr>
2023-10-25 10:41:17 +0200

    suite

--- LightOff_MAUMEJEAN_version_console/src/lightoff_maumejean_version_console/GrilleDeJeu.java
+++ LightOff_MAUMEJEAN_version_console/src/lightoff_maumejean_version_console/GrilleDeJeu.java
@@ -5,6 +5,9 @@
 package lightoff_maumejean_version_console;
 
 /**
+ * La classe GrilleDeJeu représente une grille de cellules lumineuses.
+ * Cette grille est constituée de cellules lumineuses organisées en lignes 
+ * et colonnes.
  * classe qui offre des fonctionnalités pour générer, activer, éteindre et 
  * afficher l'état de ces cellules de manière structurée.
  * @author MAUMEJEAN DENIS
@@ -14,6 +17,11 @@ public class GrilleDeJeu {
     private int nbLignes;
     private int nbColonnes;
 
+    /**
+     * Constructeur de la classe GrilleDeJeu.
+     * @param p_nbLignes Le nombre de lignes de la grille.
+     * @param p_nbColonnes Le nombre de colonnes de la grille.
+     */
     public GrilleDeJeu(int p_nbLignes, int p_nbColonnes) {
         nbLignes = p_nbLignes;
         nbColonnes = p_nbColonnes;
@@ -26,6 +34,9 @@ public class GrilleDeJeu {
         }
     }
 
+    /**
+     * Éteint toutes les cellules de la grille.
+     */
     public void eteindreToutesLesCellules() {
         for (int i = 0; i < nbLignes; i++) {
             for (int j = 0; j < nbColonnes; j++) {
@@ -34,7 +45,11 @@ public class GrilleDeJeu {
         }
     }
 
-    // Ajoutez ici les autres méthodes selon vos spécifications.
+    /**
+     * Redéfinition de la méthode toString pour afficher l'état de la grille 
+     * sous forme d'une représentation visuelle organisée.
+     * @return Une chaîne de caractères représentant l'état de la grille.
+     */
 
     @Override
     public String toString() {
