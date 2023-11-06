bc51c71510b8b1ecbbb908647a844ef7f6d99348
Denis MAUMEJEAN <denis.maumejean@epfedu.fr>
2023-11-06 14:57:07 +0100

    debut

---
+++ LightOff_MAUMEJEAN_version_graphique/src/lightoff_maumejean_version_console/CelluleGraphique.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lightoff_maumejean_version_console;
+
+/**
+ *
+ * @author MAUMEJEAN DENIS
+ */
+public class CelluleGraphique extends JButton {
+    
+}
